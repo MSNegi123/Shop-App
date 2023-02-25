306378d04a8d654d67a9cfc518869df363397b6b
Mehul Negi <mehul.negi@decimal.co.in>
2023-02-26 03:17:44 +0530

    way for shrinking down the minimal changes in tree by using consumer and Provider.of(context)

--- lib/widgets/productItem.dart
+++ lib/widgets/productItem.dart
@@ -7,38 +7,38 @@ import '../constants/constants.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final _product = Provider.of<ProductModel>(context);
+    final _product = Provider.of<ProductModel>(context, listen: false);
     return
         //Consumer(builder: builder,_product,_child)=> ClipRRect(
-        Consumer<ProductModel>(  // Consumer<ProductModel> = Provider.of<ProductModel>(context)
-            builder: (ctx, _product, _) => ClipRRect(
-                  child: GridTile(
-                    child: GestureDetector(
-                      onTap: () => Navigator.of(context).pushNamed(
-                          Routes.productDetailScreenRoute,
-                          arguments: _product.id),
-                      child: Image.network(
-                        _product.imageUrl,
-                        fit: BoxFit.cover,
-                        // label: _child,
-                      ),
-                    ),
-                    footer: GridTileBar(
-                        backgroundColor: Colors.black87,
-                        title: Text(_product.title),
-                        leading: IconButton(
-                            icon: Icon(_product.isFavourite
-                                ? Icons.favorite_border
-                                : Icons.favorite),
-                            color: Theme.of(context).accentColor,
-                            onPressed: _product.toggleFavouriteStatus),
-                        trailing: IconButton(
-                            icon: Icon(Icons.shopping_cart),
-                            color: Theme.of(context).accentColor,
-                            onPressed: () {})),
-                  ),
-                ),
-        // child: Text('Helper Text'),
-        );
+        ClipRRect(
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () => Navigator.of(context).pushNamed(
+              Routes.productDetailScreenRoute,
+              arguments: _product.id),
+          child: Image.network(
+            _product.imageUrl,
+            fit: BoxFit.cover,
+            // label: _child,
+          ),
+        ),
+        footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            title: Text(_product.title),
+            leading: Consumer<ProductModel>(
+              builder: (ctx, _product, _) => IconButton(
+                  icon: Icon(_product.isFavourite
+                      ? Icons.favorite_border
+                      : Icons.favorite),
+                  color: Theme.of(context).accentColor,
+                  onPressed: _product.toggleFavouriteStatus),
+            ),
+            trailing: IconButton(
+                icon: Icon(Icons.shopping_cart),
+                color: Theme.of(context).accentColor,
+                onPressed: () {})),
+      ),
+    );
+    // child: Text('Helper Text'),
   }
 }
